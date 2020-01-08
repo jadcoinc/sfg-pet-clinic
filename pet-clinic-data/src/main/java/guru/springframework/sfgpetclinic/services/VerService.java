08614332c8ea774c5f303cc3b9b160b06f79f1fc
ed <ed@somecompany.com>
2020-01-07 21:17:23 -0500

    implemented map-based service Owner, Pet and Vet classes

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VerService.java
@@ -1,17 +1,9 @@
 package guru.springframework.sfgpetclinic.services;
 
-import java.util.Set;
-
 import guru.springframework.sfgpetclinic.model.Vet;
 
-public interface VerService {
-
+public interface VerService extends CrudService<Vet, Long> {
 
 	Vet findByLastName(String lastName);
-	
-	Vet byndById(Long id);
-	
-	Vet save(Vet Vet);
-	
-	Set<Vet> findAll();
+
 }
