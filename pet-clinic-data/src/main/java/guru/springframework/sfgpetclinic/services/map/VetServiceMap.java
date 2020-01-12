b6a85ff00aba5cc7adae71fc3184cd1bd965dc0e
ed <ed@somecompany.com>
2020-01-12 11:19:35 -0500

    created bootstrap class to load test data

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -3,32 +3,38 @@ package guru.springframework.sfgpetclinic.services.map;
 import java.util.Set;
 
 import guru.springframework.sfgpetclinic.model.Vet;
-import guru.springframework.sfgpetclinic.services.CrudService;
+import guru.springframework.sfgpetclinic.services.VetService;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
-	@Override
-	public Set<Vet> findAll() {
-		return super.findAll();
-	}
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
 
-	@Override
-	public Vet findById(Long id) {
-		return super.findById(id);
-	}
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
 
-	@Override
-	public Vet save(Vet object) {
-		return super.save(object.getId(), object);
-	}
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
 
-	@Override
-	public void delete(Vet object) {
-		super.delete(object);
-	}
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
 
-	@Override
-	public void deleteById(Long id) {
-		super.deleteById(id);
- 	}
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Vet findByLastName(String lastName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
