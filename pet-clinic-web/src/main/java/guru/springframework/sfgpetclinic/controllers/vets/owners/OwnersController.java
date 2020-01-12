a8566f13aab27bdcda8f878d68e80d35bbe761f8
ed <ed@somecompany.com>
2020-01-12 14:38:18 -0500

    modified Owners controller to utilize OwnerService and return data

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/vets/owners/OwnersController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/vets/owners/OwnersController.java
@@ -5,14 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
+
 @Controller
 @RequestMapping(value = "/owners")
 public class OwnersController {
 
-	@RequestMapping(value = {"", "/", "/index", "index.html"}, method = RequestMethod.GET)
-	public String listOwners(Model model) {
-		model.addAttribute("ownersText", "List of Owners");
-		return "owners/index";
-	}
-	
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping(value = {"", "/", "/index", "index.html"}, method = RequestMethod.GET)
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+        return "owners/index";
+    }
+
 }
