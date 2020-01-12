a912cb8456ba5c6383d76126a2fe36eb339148c5
ed <ed@somecompany.com>
2020-01-12 15:11:27 -0500

    modified Vets controller to use VetService to display data

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/vets/VetsController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/vets/VetsController.java
@@ -5,13 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import guru.springframework.sfgpetclinic.services.VetService;
+
 @Controller
 public class VetsController {
 
-	@RequestMapping(value = {"/vets", "/vets/index", "/vets/index.html"}, method = RequestMethod.GET)
-	public String listVets(Model model) {
-		model.addAttribute("vetsText", "List of Vets");
-		return "vets/index";
-	}
-	
+    private final VetService vetService;
+    
+    public VetsController(VetService vetService) {
+        super();
+        this.vetService = vetService;
+    }
+
+    @RequestMapping(value = {"/vets", "/vets/index", "/vets/index.html"}, method = RequestMethod.GET)
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+        return "vets/index";
+    }
+
 }
